49f83a65672af13c0f8937564b8168626aa3153a
zivanovicj <zivanovicjelena000@gmail.com>
2023-06-24 15:39:31 +0200

    Send mail implementation when seller status changes

--- WebProjekat/WebProjekat/Services/AdminService.cs
+++ WebProjekat/WebProjekat/Services/AdminService.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using WebProjekat.Common;
 using WebProjekat.DTO.UserDTO;
 using WebProjekat.Infrastructure;
@@ -38,7 +41,44 @@ namespace WebProjekat.Services
                 return false;
             seller.Approved = status;
             _adminRepository.SetSellerStatus(seller);
+
+            var vr = status.ToString();
+            var apiKey = "E768E081003B29BF53422EC3597F30618F3E05C3A893FEFE1118B10338F1D41724E4A0488A9C06372E3A6819FCA263C0";
+            var recipientEmail = email;
+            var subject = "Status of verification";
+            var bodyHtml = $"<h1>Hello, your status of verification is</h1><p>{vr}</p>";
+            SendEmail(apiKey, recipientEmail, subject, bodyHtml);
+
             return true;
+        }
+
+        private static void SendEmail(string apiKey, string recipientEmail, string subject, string body)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.elasticemail.com/v2/");
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var requestContent = new StringContent(
+                    $"apikey={Uri.EscapeDataString(apiKey)}&from=zivanovicj71@gmail.com&to=" +
+                    $"{Uri.EscapeDataString(recipientEmail)}&subject={Uri.EscapeDataString(subject)}&bodyHtml=" +
+                    $"{Uri.EscapeDataString(body)}",
+                    Encoding.UTF8,
+                    "application/x-www-form-urlencoded"
+                );
+
+                var response = client.PostAsync("email/send", requestContent).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Email sent successfully!");
+                }
+                else
+                {
+                    var errorResponse = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("Failed to send email. Error message: " + errorResponse);
+                }
+            }
         }
     }
 }
