d1e758ab1e93d7c55a5184494fc65021ba2e070a
zivanovicj <zivanovicjelena000@gmail.com>
2023-07-01 16:11:27 +0200

    Removed default page and added navbar

--- web-projekat-ui/src/App.js
+++ web-projekat-ui/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation></Navigation>
     </div>
   );
 }
---
+++ web-projekat-ui/src/components/Navigation.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Navigation(){
+    return (
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>Web Shop</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Register</Nav.Link>
+            <Nav.Link href="#features">Log In</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      );
+}
+
+export default Navigation;
